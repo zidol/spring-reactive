bfc32396563e9586a0d04fd7f41c6ee0e5237b58
zidol <ajh3166@naver.com>
2024-12-27 17:17:07 +0900

    Scheduler를 위한 전용 Operator, Parallel 에제 코드

---
+++ src/main/java/com/zidol/springreactive/example/parallel/ParallelExample02.java
@@ -0,0 +1,17 @@
+package com.zidol.springreactive.example.parallel;
+
+import com.zidol.springreactive.utils.Logger;
+import com.zidol.springreactive.utils.TimeUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class ParallelExample02 {
+    public static void main(String[] args) {
+        Flux.fromArray(new Integer[]{1, 3, 5, 7, 9, 11, 13, 15})
+                .parallel()
+                .runOn(Schedulers.parallel())
+                .subscribe(Logger::onNext);
+        TimeUtils.sleep(100L);
+
+    }
+}
