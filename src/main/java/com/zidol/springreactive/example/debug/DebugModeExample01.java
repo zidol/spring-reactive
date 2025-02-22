2596b746e80544fc502a56f8819f430d7c43369e
zidol <ajh3166@naver.com>
2025-02-22 18:40:30 +0900

    Feat : debug mode with onOperatorDebug

---
+++ src/main/java/com/zidol/springreactive/example/debug/DebugModeExample01.java
@@ -0,0 +1,15 @@
+package com.zidol.springreactive.example.debug;
+
+import com.zidol.springreactive.utils.Logger;
+import reactor.core.publisher.Flux;
+
+/**
+ * Non-Debug mode
+ */
+public class DebugModeExample01 {
+    public static void main(String[] args) {
+        Flux.just(2, 4, 6, 8)
+                .zipWith(Flux.just(1, 2, 3, 0), (x, y) -> x/y)
+                .subscribe(Logger::onNext, Logger::onError);
+    }
+}
